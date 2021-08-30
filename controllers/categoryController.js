49e33cbe5efe7605654d6371813e1f99fed0d2c7
Jonathan Park <johanpk10@gmail.com>
2021-08-29 17:20:31 -0700

    Implement Category create, update, and delete controllers

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,4 @@
+var { body, validationResult } = require('express-validator');
 var Category = require('../models/category');
 var Item = require('../models/item');
 
@@ -29,7 +30,7 @@ exports.category_detail = function (req, res, next) {
         var [ category, item_list ] = results;
 
         if (category === null) {
-            var err = new Error('Category not found');
+            let err = new Error('Category not found');
             err.status = 404;
             return next(err);
         }
@@ -42,28 +43,145 @@ exports.category_detail = function (req, res, next) {
 };
 
 //Create Category
-exports.category_create_get = function (req, res) {
-    res.send('Create category?');
+exports.category_create_get = function (req, res, next) {
+    res.render('category_form', { title: 'New Category' });
 };
 
-exports.category_create_post = function (req, res) {
-    res.send('Category created!');
-};
+exports.category_create_post = [
+    //validate and sanitize fields
+    body('name').trim().isLength({ min: 1 }).withMessage('Name must not be empty')
+        .isLength({ max: 100 }).withMessage('Name cannot be more than 100 characters')
+        .escape(),
+    body('description').trim().isLength({ min: 1 }).withMessage('Description must not be empty')
+        .escape(),
+
+    function (req, res, next) {
+        var newCategory = new Category(
+            {
+                name: req.body.name,
+                description: req.body.description
+            }
+        );
+
+        var errors = validationResult(req);
+
+        //if failed validation, render form with previous values and error messages
+        if (!errors.isEmpty()) {
+            res.render('category_form', { title: 'New Category', category: newCategory, errors: errors.array() });
+        } else {
+            //if passed validation, save category to db and redirect to new category detail page
+            newCategory.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+
+                res.redirect(newCategory.url);
+            });
+        }
+    }
+];
 
 //Update Category
-exports.category_update_get = function (req, res) {
-    res.send('Update category?');
+exports.category_update_get = function (req, res, next) {
+    Category.findById(req.params.id).exec(function(err, category) {
+        if (err) {
+            return next(err);
+        }
+
+        if (category === null) {
+            let err = new Error('Category not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('category_form', { title: 'Update Category', category });
+    });
 };
 
-exports.category_update_post = function (req, res) {
-    res.send('Category updated!');
-};
+exports.category_update_post = [
+    //validate and sanitize fields
+    body('name').trim().isLength({ min: 1 }).withMessage('Name must not be empty')
+        .isLength({ max: 100 }).withMessage('Name cannot be more than 100 characters')
+        .escape(),
+    body('description').trim().isLength({ min: 1 }).withMessage('Description must not be empty')
+        .escape(),
+    
+    function (req, res, next) {
+        var updatedCategory = new Category(
+            {
+                name: req.body.name,
+                description: req.body.description,
+                _id: req.params.id
+            }
+        );
+
+        var errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('category_form', { title: 'Update Category', category: updatedCategory, errors: errors.array() });
+        } else {
+            Category.findByIdAndUpdate(req.params.id, updatedCategory, function (err, category) {
+                if (err) {
+                    return next(err);
+                }
+
+                res.redirect(category.url);
+            });
+        }
+    }
+];
 
 //Delete Category
-exports.category_delete_get = function (req, res) {
-    res.send('Delete category?');
+exports.category_delete_get = function (req, res, next) {
+    var categoryId = req.params.id;
+
+    Promise.all(
+        [
+            Category.findById(categoryId),
+            Item.find({ category: categoryId })
+        ]
+    )
+    .then(function (results) {
+        var [ category, item_list ] = results;
+
+        if (category === null) {
+            let err = new Error('Category not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('category_delete', { title: 'Delete Category', category, item_list });
+    })
+    .catch(function (err) {
+        return next(err);
+    });  
 };
 
-exports.category_delete_post = function (req, res) {
-    res.send('Category deleted!');
+exports.category_delete_post = function (req, res, next) {
+    var categoryId = req.body.categoryid;
+
+    Promise.all(
+        [
+            Category.findById(categoryId),
+            Item.find({ category: categoryId })
+        ]
+    )
+    .then(function (results) {
+        var [ category, item_list ] = results;
+
+        if (item_list.length > 0) {
+            res.render('category_delete', { title: 'Delete Category', category, item_list });
+        } else {
+            Category.findByIdAndDelete(categoryId, function (err, category) {
+                if (err) {
+                    return next(err);
+                }
+
+                res.redirect('/catalog');
+            });
+        }
+    })
+    .catch(function (err) {
+        return next(err);
+    });  
 };
